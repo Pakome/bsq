18179fee401a6c6add127ccb4e00eae9c97ca4a8
Pakome <maxime.lardier@epitech.eu>
2018-09-06 10:13:35 +0200

    transformed raw php to clean class

--- Bot.php
+++ Bot.php
@@ -1,64 +1,105 @@
 <?php
 
-require_once('colorClass.php');
-
 class Bot {
 
-	public $colors = new Colors();
+	private $errors = [];
+	private $foreground_colors = array();
+	private $background_colors = array();
 
-	public function __construct() {
-		// Start the timer of the script
-		$timestamp_start = microtime(true);
+	public function __construct($primary_map_color = "black", $background_map_color = "blue", $square_color = "red") {
+		// Set up shell colors
+		$this->foreground_colors['black'] = '0;30';
+		$this->foreground_colors['dark_gray'] = '1;30';
+		$this->foreground_colors['blue'] = '0;34';
+		$this->foreground_colors['light_blue'] = '1;34';
+		$this->foreground_colors['green'] = '0;32';
+		$this->foreground_colors['light_green'] = '1;32';
+		$this->foreground_colors['cyan'] = '0;36';
+		$this->foreground_colors['light_cyan'] = '1;36';
+		$this->foreground_colors['red'] = '0;31';
+		$this->foreground_colors['light_red'] = '1;31';
+		$this->foreground_colors['purple'] = '0;35';
+		$this->foreground_colors['light_purple'] = '1;35';
+		$this->foreground_colors['brown'] = '0;33';
+		$this->foreground_colors['yellow'] = '1;33';
+		$this->foreground_colors['light_gray'] = '0;37';
+		$this->foreground_colors['white'] = '1;37';
+
+		$this->background_colors['black'] = '40';
+		$this->background_colors['red'] = '41';
+		$this->background_colors['green'] = '42';
+		$this->background_colors['yellow'] = '43';
+		$this->background_colors['blue'] = '44';
+		$this->background_colors['magenta'] = '45';
+		$this->background_colors['cyan'] = '46';
+		$this->background_colors['light_gray'] = '47';
 	}
 
 	public function __destruct() {
-		// Calculate the time of execution
-		$timestamp_end = microtime(true);
-		$difference_ms = $timestamp_end - $timestamp_start;
-		echo $colors->getColoredString("Time of execution : " . $difference_ms . ' milliseconds.', "black", "green") . "\n";
+		foreach ($this->errors as $error) {
+			echo $this->getColoredString("There was an error: $error", "black", "red") . "\n";
+		}
 	}
 
 	// Verify if an argument is provided
-	function checkArgv() {
-		global $errors;
+	public function checkArgv() {
 		global $argc;
 		if ($argc == 1) {
-			return 0;
-		} else if ($argc > 2) {
-			$errors[] = "Too much parameters given.";
 			return 1;
-		} else {
+		} else if ($argc == 2) {
+			return 2;
+		} else if ($argc > 2) {
+			$this->errors[] = "too much parameters given.";
+			return 3;
+		}
+	}
+
+	// Returns colored string
+	public function getColoredString($string, $foreground_color = null, $background_color = null) {
+		$colored_string = "";
+
+		// Check if given foreground color found
+		if (isset($this->foreground_colors[$foreground_color])) {
+			$colored_string .= "\033[" . $this->foreground_colors[$foreground_color] . "m";
+		}
+		// Check if given background color found
+		if (isset($this->background_colors[$background_color])) {
+			$colored_string .= "\033[" . $this->background_colors[$background_color] . "m";
+		}
+
+		// Add string and end coloring
+		$colored_string .=  $string . "\033[0m";
+
+		return $colored_string;
+	}
+
+	// Returns all foreground color names
+	public function getForegroundColors() {
+		return array_keys($this->foreground_colors);
+	}
+
+	// Returns all background color names
+	public function getBackgroundColors() {
+		return array_keys($this->background_colors);
+	}
+
+	// Diplay Commands 
+	public function displayCommands() {
+		$welcome = readline("Welcome on BSQ Solver® press any character to continue or 'enter' quit to exit the program. \n");
+		if ($welcome == "quit" || $welcome == "q") {
+			return false;
+		}
+		$choice = readline("You can either solve an existing map or generate your own map:\n\n    -Enter the name of your existing map to solve it.\n    -Press enter to generater your own.\n\n$ ");
+		if ($choice == "") {
 			return -1;
+		} else {
+			return $choice;
 		}
-	}
-
-	// Verify that the first number of the file is equal to the number of lines in the file -1
-	function verifyMapHeight($map, $mapHeight) {
-		global $errors;
-		$lines = count(file($map)) - 1;
-		// $mapHeight = filter_var($mapHeight, FILTER_VALIDATE_INT);
-		$mapHeight = (int)$mapHeight;
-		if ($lines !== $mapHeight) {
-			$errors[] = "The number given on first line is not equal the real map's height: $mapHeight line(s) given, $lines found(s).";
-			return false;
-		}
-	}
-
-	// Verify that the value of the first line is the map's height
-	public function validatesAsInt($number) {
-		global $errors;
-	    $number = filter_var($number, FILTER_VALIDATE_INT);
-	    if (is_int($number)) {
-	    	return true;
-	    } else {
-	    	$errors[] = "Verify that the first line is the map's height and is an integer.";
-			return false;
-	    }
 	}
 
 	public function sayHello() {
 
-return $colors->getColoredString("BBBBBBBBBBBBBBBBB      SSSSSSSSSSSSSSS      QQQQQQQQQ              SSSSSSSSSSSSSSS      OOOOOOOOO     LLLLLLLLLLL     VVVVVVVV           VVVVVVVVEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR   
+return $this->getColoredString("BBBBBBBBBBBBBBBBB      SSSSSSSSSSSSSSS      QQQQQQQQQ              SSSSSSSSSSSSSSS      OOOOOOOOO     LLLLLLLLLLL     VVVVVVVV           VVVVVVVVEEEEEEEEEEEEEEEEEEEEEERRRRRRRRRRRRRRRRR   
 B::::::::::::::::B   SS:::::::::::::::S   QQ:::::::::QQ          SS:::::::::::::::S   OO:::::::::OO   L:::::::::L     V::::::V           V::::::VE::::::::::::::::::::ER::::::::::::::::R  
 B::::::BBBBBB:::::B S:::::SSSSSS::::::S QQ:::::::::::::QQ       S:::::SSSSSS::::::S OO:::::::::::::OO L:::::::::L     V::::::V           V::::::VE::::::::::::::::::::ER::::::RRRRRR:::::R 
 BB:::::B     B:::::BS:::::S     SSSSSSSQ:::::::QQQ:::::::Q      S:::::S     SSSSSSSO:::::::OOO:::::::OLL:::::::LL     V::::::V           V::::::VEE::::::EEEEEEEEE::::ERR:::::R     R:::::R
@@ -75,17 +116,8 @@ B:::::::::::::::::B S::::::SSSSSS:::::S QQ::::::::::::::Q       S::::::SSSSSS:::
 B::::::::::::::::B  S:::::::::::::::SS    QQ:::::::::::Q        S:::::::::::::::SS    OO:::::::::OO   L::::::::::::::::::::::L   V:::V           E::::::::::::::::::::ER::::::R     R:::::R
 BBBBBBBBBBBBBBBBB    SSSSSSSSSSSSSSS        QQQQQQQQ::::QQ       SSSSSSSSSSSSSSS        OOOOOOOOO     LLLLLLLLLLLLLLLLLLLLLLLL    VVV            EEEEEEEEEEEEEEEEEEEEEERRRRRRRR     RRRRRRR
                                                     Q:::::Q                                                                                                                                
-                                                     QQQQQQ", "cyan", "red") . "\n";
+                                                     QQQQQQ", "cyan", "red") . "\n \n \n";
 	}
-
-	// Print each errors
-	public function displayErrors() {
-		foreach ($errors as $error) {
-			echo $colors->getColoredString("There was an error: $error", "black", "red") . "\n";
-		}
-	}
-
-
 
 }
 
--- bsq.php
+++ bsq.php
@@ -1,135 +1,46 @@
 <?php
 
 // Start the timer of the script
-$timestamp_debut = microtime(true);
+$timestamp_start = microtime(true);
 
-include_once("algorithm.php");
-include_once("colorClass.php");
-include_once("frame_generator.php");
-include_once("main.php");
+include_once("Map.php");
+include_once("Bot.php");
 
-// Initiate the variables for errors and colors
-$errors = [];
-$colors = new Colors();
-
-// Check that there is the right number of arguments given
-// $checkArguments = checkArgv();
-
-// Get the map from the file and put it in an array
-function getMap($numberLine, $file) {
-	$arr_map = [];
-	$map = fopen($file, 'r+');
-	for ($i = 0; $i <= $numberLine; $i++) {
-    	$line = fgets($map);
-    	$full_line = str_split($line);
-    	$arr_map[] = $full_line;
-    }
-    array_shift($arr_map);
-    fclose($map);
-    return $arr_map;
-}
-
-// Get the value of the first lign of the map,that the file exists and that there is no errors
-function getNumberLines($file) {
-	global $errors;
-    if (file_exists($file)) {
-        $map = fopen($file, 'r+');
-        $number_of_lines = fgets($map);
-        fclose($map);
-        if (validatesAsInt($number_of_lines)) {
-        	if(!verifyMapHeight($file, $number_of_lines)) {
-        		return $number_of_lines;
-        	}
-        }      
- 	}
-    else {
-        $errors[] = "The file $file does not exist.";
-        return;
-    }
-}
-
-// Formate the map, change "." by 1's and "o" by 0's so the algorithm can find the biggest square
-function transformMap($arr) {
-	$formated_map = $arr;
-	for ($i = 0; $i < count($arr); $i++) {
-		for ($j = 0; $j < count($arr[$i]); $j++) {
-			if ($formated_map[$i][$j] == ".") {
-				$formated_map[$i][$j] = preg_replace("/\./", 1, $arr[$i][$j]);
-			}
-			else if ($formated_map[$i][$j] == "o") {
-				$formated_map[$i][$j] = preg_replace("/o/", 0, $arr[$i][$j]);
-			}
-
-		}
-	}
-	return $formated_map;
-}
-
-// Verify that the value of the first line is the map's height
-function validatesAsInt($number) {
-	global $errors;
-    $number = filter_var($number, FILTER_VALIDATE_INT);
-    if (is_int($number)) {
-    	return true;
-    } else {
-    	$errors[] = "Verify that the first line is the map's height and is an integer.";
-		return false;
-    }
-}
-
-// Verify if an argument is provided
-function checkArgv() {
-	global $errors;
-	global $argc;
-	if ($argc == 1) {
-		return 0;
-	} else if ($argc > 2) {
-		$errors[] = "Too much parameters given.";
-		return 1;
-	} else {
-		return -1;
-	}
-}
-
-// Verify that the first number of the file is equal to the number of lines in the file -1
-function verifyMapHeight($map, $mapHeight) {
-	global $errors;
-	$lines = count(file($map)) - 1;
-	// $mapHeight = filter_var($mapHeight, FILTER_VALIDATE_INT);
-	$mapHeight = (int)$mapHeight;
-	if ($lines !== $mapHeight) {
-		$errors[] = "The number given on first line is not equal the real map's height: $mapHeight line(s) given, $lines found(s).";
-		return false;
-	}
-}
-
-// Delete every "\n" at the end of each arrays
-function deleteJumpLine($arr) {
-	for ($i = 0; $i < count($arr); $i++) {
-		array_pop($arr[$i]);
-	}
-	return $arr;
-}
+// Initiate a new instance of Bot;
+$bot = new Bot();
 
 // Display the logo
-require_once("welcome.php");
+echo $bot->sayHello();
+$arg = $bot->checkArgv();
 
-// If the right number of argument was given, start the main function
-if (checkArgv() == 0) {
-	generateMap();
-	mainFunction("map.txt");
-} else if (checkArgv() == -1) {
-	mainFunction($argv[1]);
-}
-
-// Print each errors
-foreach ($errors as $error) {
-	echo $colors->getColoredString("There was an error: $error", "black", "red") . "\n";
+if ($arg < 2 && $arg == 1) {
+	$choice = $bot->displayCommands();
+	if ($choice !== false) {
+		if ($choice == -1) {
+			$map = new Map('map.txt');
+			$map->generateMap();
+			$numberOfLines = $map->getNumberLines();
+			$arr_map = $map->getMap();
+			$formated_map = $map->transformMap();
+			echo $bot->getColoredString("The biggest square is " . $map->defineBsq() . " tiles larges." , "black", "green") . "\n";
+		} else {
+			$map = new Map($choice);
+			$numberOfLines = $map->getNumberLines();
+			$arr_map = $map->getMap();
+			$formated_map = $map->transformMap();
+			echo $bot->getColoredString("The biggest square is " . $map->defineBsq() . " tiles larges." , "black", "green") . "\n";
+		}
+	}
+} else if ($arg == 2) {
+	$map = new Map($argv[1]);
+	$numberOfLines = $map->getNumberLines();
+	$arr_map = $map->getMap();
+	$formated_map = $map->transformMap();
+	echo $bot->getColoredString("The biggest square is " . $map->defineBsq() . " tiles larges." , "black", "green") . "\n";
 }
 
 // Calculate the time of execution
-$timestamp_fin = microtime(true);
-$difference_ms = $timestamp_fin - $timestamp_debut;
-echo $colors->getColoredString("Time of execution : " . $difference_ms . ' milliseconds.', "black", "green") . "\n";
+$difference_ms = microtime(true) - $timestamp_start;
+echo $bot->getColoredString("Time of execution : " . $difference_ms . ' milliseconds.', "black", "green") . "\n";
 
 ?>
